9216c043fbbaf17f98ad8b92f49c03244d558302
Mr2Much <anubis.lockward@gmail.com>
2018-03-14 06:32:40 -0400

    -Added constructors to class MathEquation.
    -Removed static method create() from Main class.

--- src/com/lockward/anubis/Main.java
+++ src/com/lockward/anubis/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         MathEquation[] equations = new MathEquation[4];
 
-        equations[0] = create(100.0, 50.0, 'd');
-        equations[1] = create(25.0, 92.0, 'a');
-        equations[2] = create(225.0, 17.0, 's');
-        equations[3] = create(11.0, 3.0, 'm');
+        equations[0] = new MathEquation('d', 100.0, 50.0);
+        equations[1] = new MathEquation('a', 25.0, 92.0);
+        equations[2] = new MathEquation('s', 225.0, 17.0);
+        equations[3] = new MathEquation('m', 11.0, 3.0);
 
         for (MathEquation equation : equations) {
             equation.execute();
@@ -17,13 +17,13 @@ public class Main {
         }
     }
 
-    public static MathEquation create(double leftVal, double rightVal, char opCode) {
-        MathEquation equation = new MathEquation();
-
-        equation.setLeftVal(leftVal);
-        equation.setRightVal(rightVal);
-        equation.setOpCode(opCode);
-
-        return equation;
-    }
+//    public static MathEquation create(double leftVal, double rightVal, char opCode) {
+//        MathEquation equation = new MathEquation();
+//
+//        equation.setLeftVal(leftVal);
+//        equation.setRightVal(rightVal);
+//        equation.setOpCode(opCode);
+//
+//        return equation;
+//    }
 }
--- src/com/lockward/anubis/MathEquation.java
+++ src/com/lockward/anubis/MathEquation.java
@@ -3,7 +3,7 @@ package com.lockward.anubis;
 public class MathEquation {
     private double leftVal;
     private double rightVal;
-    private char opCode;
+    private char opCode = 'a';
     private double result;
 
     public double getLeftVal() { return leftVal; }
@@ -14,6 +14,21 @@ public class MathEquation {
     public void setOpCode( char opCode ) { this.opCode = opCode; }
 
     public double getResult() { return result; }
+
+    public MathEquation() {
+
+    }
+
+    public MathEquation( char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation( char opCode, double leftVal, double rightVal) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
+
 
     public void execute() {
         switch (opCode) {
